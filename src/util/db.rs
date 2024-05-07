9628cd02c8d956d1281a38cc8a7bd9b674e6a2e2
firecoder1 <github@skyfall.33mail.com>
2024-05-07 12:05:12 +0000

    Somewhat fix the build errors

--- src/util/db.rs
+++ src/util/db.rs
@@ -2,16 +2,32 @@ use color_eyre::Result;
 use poise::serenity_prelude as serenity;
 use sqlx::PgPool;
 
-pub fn get_user_balances(user_id: serenity::UserId, guild_id: serenity::GuildId) -> Result<Record> {
-    Ok(sqlx::query!(
-        "
-SELECT wallet_balance
-FROM users
-WHERE user_id = $1 AND guild_id = $2
-    ",
-        user_id.to_string(),
-        guild_id.to_string()
-    )
-    .fetch_one(db)
-    .await?)
+pub struct UserBalances {
+    pub bank_balance: i32,
+    pub wallet_balance: i32,
+}
+
+impl UserBalances {
+    pub async fn from_user_and_guild_ids(
+        user_id: serenity::UserId,
+        guild_id: serenity::GuildId,
+        db: PgPool
+    ) -> Result<Self> {
+        let record = sqlx::query!(
+            "
+    SELECT bank_balance, wallet_balance
+    FROM users
+    WHERE user_id = $1 AND guild_id = $2
+        ",
+            user_id.to_string(),
+            guild_id.to_string()
+        )
+        .fetch_one(db)
+        .await?;
+
+        Ok(Self {
+            bank_balance: record.bank_balance,
+            wallet_balance: record.wallet_balance,
+        })
+    }
 }
